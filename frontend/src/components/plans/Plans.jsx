36603166b11a4d289e4a488464e2e5f85d8506c9
samueltilahun <samueltilahun991@gmail.com>
2024-06-22 09:17:34 +0300

    update

--- frontend/src/components/plans/Plans.jsx
+++ frontend/src/components/plans/Plans.jsx
@@ -1,20 +1,70 @@
 import React from 'react'
+import { plansData } from './plansData'
 import './plans.css'
 
 const Plans = () => {
   return (
     <div className="plans">
-        <span>ready to start <span> your journey </span> now with us</span>
+        <div className='plans-title'>
+        <span>ready to start <span className='stroke-text'> your journey </span> with us</span></div>
         <div className="plan-cards">
-            <div className="first-card">
-
+            {plansData.map((plans, index) => (
+                <div className={`cards ${index === 1 ? 'special-style' : ''}`} key={plans.id}>
+                    <i className={`icon ${plans.icon}`}></i>
+                    <div>
+                        <span>{plans.planType}</span>
+                    </div>
+                    <div>
+                        <span>{plans.money}</span>
+                    </div>
+                    <div>
+                        <i className='fas fa-check-circle'></i>
+                        <span>{plans.duration}</span>
+                    </div>
+                    <div>
+                        <i className='fas fa-check-circle'></i>
+                        <span>{plans.extra}</span>
+                    </div>
+                    <div>
+                        <i className='fas fa-check-circle'></i>
+                        <span>{plans.access}</span>
+                    </div>
+                    <div className='see-more'>
+                    <span>See more benefits</span> <i className='fas fa-arrow-right'></i>
+                    </div> 
+                    <button className='btn'>Join Now</button>
+                </div>
+            ))}
+            {/* <div className="first-card">
+                <i className=''></i>
+                <div><img src={tick} alt="" /><span></span></div>
+                <div><img src={tick} alt="" /><span>2500 BIRR</span></div>
+                <div><img src={tick} alt="" /><span>2 hours exercise</span></div>
+                <div><img src={tick} alt="" /><span>Free consultation from coaches</span></div>
+                <div><img src={tick} alt="" /><span>Access to the Community</span></div>
+                <span>See more benefits <i className='fas fa-arrow-right'></i></span>
+                <button>Join Now</button>
             </div>
             <div className="second-card">
-
+                <i className=''></i>
+                <div><img src={tick} alt="" /><span>BASIC PLAN</span></div>
+                <div><img src={tick} alt="" /><span>2500 BIRR</span></div>
+                <div><img src={tick} alt="" /><span>2 hours exercise</span></div>
+                <div><img src={tick} alt="" /><span>Free consultation from coaches</span></div>
+                <div><img src={tick} alt="" /><span>Access to the Community</span></div>
+                <span>See more benefits <i className='fas fa-arrow-right'></i></span>
+                <button>Join Now</button>
             </div>
             <div className="third-card">
-
-            </div>
+                <i className='fas fa-gem'></i>
+                <div><img src={tick} alt="" /><span>BASIC PLAN</span></div>
+                <div><img src={tick} alt="" /><span>2500 BIRR</span></div>
+                <div><img src={tick} alt="" /><span>2 hours exercise</span></div>
+                <div><img src={tick} alt="" /><span>Free consultation from coaches</span></div>
+                <div><img src={tick} alt="" /><span>Access to the Community</span></div>
+                <span>See more benefits <i className='fas fa-arrow-right'></i></span>
+                <button>Join Now</button>
+            </div> */}
         </div>
     </div>
   )
---
+++ frontend/src/components/plans/plansData.jsx
@@ -0,0 +1,30 @@
+export const plansData = [
+    {
+        id: 1,
+        icon: "fas fa-user",
+        planType: "BASIC PLAN",
+        money: "3000 BIRR",
+        duration: "1 HOUR EXERCISE",
+        extra: "Free consultation of coaches",
+        access: "Access to the community"
+    },
+    {
+        id: 2,
+        icon: "fas fa-crown",
+        planType: "PREMIUM PLAN",
+        money: "4000 BIRR",
+        duration: "2 HOURS EXERCISE",
+        extra: "Free consultation of coaches",
+        access: "Access to minibar"
+    },
+    {
+        id: 3,
+        icon: "fas fa-gem",
+        planType: "PRO PLAN",
+        money: "6000 BIRR",
+        duration: "4 HOURS EXERCISE",
+        extra: "consulatation of private coach",
+        access: "Free fitness merchandises"
+    },
+
+]
